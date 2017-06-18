05b46064a9e9c53248104be2cebd11c19f99dd03
Israel Ponce <Israel Ponce>
2017-06-18 13:07:31 -0400

    fixed gridlines on bar graph

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,15 +17,15 @@ var barChart = new Chart (CHART, {
                 'rgba(206, 83, 7, 0.8)'
 
             ],
-            borderColor: [
-                'rgba(#613232)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-
-            ],
+            // borderColor: [
+            //     'rgba(#613232)',
+            //     'rgba(54, 162, 235, 1)',
+            //     'rgba(255, 206, 86, 1)',
+            //     'rgba(75, 192, 192, 1)',
+            //     'rgba(153, 102, 255, 1)',
+            //     'rgba(255, 159, 64, 1)'
+            //
+            // ],
             borderWidth: 2
         }]
     },
@@ -39,7 +39,9 @@ var barChart = new Chart (CHART, {
                     },
                     maxTicksLimit: 5,
                     suggestedMax: 100
-
+                },
+                gridLines: {
+                    color: "grey"
                 }
             }]
         },
